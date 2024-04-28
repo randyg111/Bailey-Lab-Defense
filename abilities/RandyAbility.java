212e6b363f58effa0db144689c61c14e5481d09f
RiceBowlFullOfRice <168307381+RiceBowlFullOfRice@users.noreply.github.com>
2024-04-28 03:53:47 -0500

    Delete abilities directory

--- abilities/RandyAbility.java
+++
@@ -1,9 +0,0 @@
-package abilities;
-
-public class RandyAbility extends Ability{
-    static final int d = 0;
-    static final double r = 0;
-    public RandyAbility(){
-        super(d, r);
-    }
-}
--- abilities/ZhengAbility.java
+++
@@ -1,9 +0,0 @@
-package abilities;
-
-public class ZhengAbility extends Ability{
-    static final int d = 1800;
-    static final double r = 3;
-    public ZhengAbility(){
-        super(d, r);
-    }
-}
